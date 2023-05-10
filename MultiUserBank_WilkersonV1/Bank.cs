149c81de6166abbe9f51957cab9504afed20e770
jonathan wilkerson <jonathan.wilkerson@student.centralia.edu>
2023-05-09 21:37:47 -0700

    YES

--- MultiUserBank_WilkersonV1/Bank.cs
+++ MultiUserBank_WilkersonV1/Bank.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using MultiUserBank_WilkersonV1;
+using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata;
 
 namespace MultiUserBank_WilkersonV1
 {
@@ -64,3 +67,31 @@ namespace MultiUserBank_WilkersonV1
         }
     }
 }
+
+
+
+//My notes and explaintion
+
+//This code is a class called `Bank` for the bank application, which keeps track of the bank's balance and manages the bank's customers. It allows customers to log in, deposit, withdraw, and retrieve the username and balance of a customer. Here's a step-by-step explanation:
+
+//1. `private const decimal InitialBankBalance = 10000m;` -This is a private constant value that represents the initial balance of the bank, which is 10,000 in decimal format.
+
+//2. `private decimal _bankBalance; private List<Customer> _customers;` -These are private fields for the bank's balance and a list of customers.
+
+//3. `public Bank()` -This is the constructor of the `Bank` class. When a new instance of `Bank` is created, the bank's balance is set to the initial bank balance, and the `InitializeCustomers` method is called to create some customers.
+
+//4. `public decimal BankBalance => _bankBalance;` -This is a read - only property that allows the bank's balance to be accessed from outside the class.
+
+//5. `private void InitializeCustomers()` -This is a private method that initializes the `_customers` list with some `Customer` objects. Each `Customer` is created with a username, password, and initial balance.
+
+//6. `public int Login(string username, string password)` -This is a method that allows a customer to log in. It checks each customer in the `_customers` list, and if the username and password match, it returns the index of the customer in the list (which acts as the customer's ID). If no match is found, it returns -1.
+
+//7. `public string GetCustomerName(int customerId)` -This method returns the username of the customer with the given ID.
+
+//8. `public decimal GetCustomerBalance(int customerId)` -This method returns the balance of the customer with the given ID.
+
+//9. `public void Deposit(int customerId, decimal amount)` -This method allows a customer to deposit an amount of money. It adds the amount to both the customer's balance and the bank's balance.
+
+//10. `public decimal Withdraw(int customerId, decimal amount)` -This method allows a customer to withdraw an amount of money. It subtracts the withdrawn amount from both the customer's balance and the bank's balance, and returns the amount that was actually withdrawn (as per the withdrawal rules in the `Customer` class).
+
+//So, this `Bank` class manages a list of customers, keeps track of the bank's balance, and handles customer logins, deposits, and withdrawals.
--- MultiUserBank_WilkersonV1/Program.cs
+++ MultiUserBank_WilkersonV1/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using static System.Formats.Asn1.AsnWriter;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics.Metrics;
+using System.Net.NetworkInformation;
+using System.Buffers.Text;
 
 namespace MultiUserBank_WilkersonV1
 {
@@ -6,10 +11,54 @@ namespace MultiUserBank_WilkersonV1
     {
         static void Main(string[] args)
         {
-            // First or Nickname Lastname
+            // John Wilkerson
+
             // IT112
-            // NOTES: MultiUserBank
-            // BEHAVIORS NOT IMPLEMENTED AND WHY:
+
+
+            // NOTES: MultiUserBank   
+
+
+            //This code is a simple console - based banking application, where users can log in, check their balance, deposit, withdraw, and log out. The bank also keeps track of its balance. I'll break down this code step by step 
+
+            //1. `Bank bank = new Bank();` -Here, an instance of the Bank class is being created.This represents the bank where all transactions will be performed.
+
+            //2. `Console.WriteLine($"Bank initial balance: {bank.BankBalance:C}");` - This prints the initial balance of the bank to the console.We don't see the BankBalance property or the Bank class in this code, but we can infer that it's a property within the Bank class that keeps track of the total amount of money in the bank.
+
+            //3. `int choice; int loggedInUserId = -1;` - Two integer variables are declared. `choice` will be used to store the user's choices in the application, while `loggedInUserId` will store the ID of the currently logged in user. It's initially set to -1, indicating no user is logged in.
+
+            //4. The outer `while (true)` loop keeps the application running indefinitely until the user chooses to exit.
+
+            //5. `if (loggedInUserId == -1)` - This checks if a user is currently logged in. If `loggedInUserId` is -1, no user is logged in.
+
+            //6. If no user is logged in, the application displays two options: 1) Login, and 2) Exit.The user's choice is read from the console and stored in the `choice` variable.
+
+            //7. If the user chooses to log in (`choice == 1`), they're prompted to enter their username and password. These are then passed to the `Login` method of the `Bank` class, which presumably checks the credentials and returns the ID of the logged user. If the username and password are incorrect, the `Login` method likely returns -1.
+
+            //8. If a user successfully logs in, a welcome message is displayed.If not, an error message is shown.
+
+            //9. If the user chooses to exit(`choice == 2`), the `break` statement exits the `while (true)` loop, effectively ending the application.
+
+            //10. If a user is logged in (`loggedInUserId != -1`), the application displays four options: 1) Check balance, 2) Deposit, 3) Withdraw, and 4) Logout.
+
+            //11. Depending on the user's choice, different actions are performed:
+
+            //    - `Check balance` - The user's balance is displayed by calling `GetCustomerBalance` with the logged in user's ID.
+
+            //    - `Deposit` - The user is prompted to enter the amount to deposit.This amount is then passed to the `Deposit` method of the `Bank` class along with the logged in user's ID. After the deposit, the new balance is displayed.
+
+            //    - `Withdraw` - The user is prompted to enter the amount to withdraw.This amount is passed to the `Withdraw` method of the `Bank` class along with the logged in user's ID. The amount actually withdrawn is then displayed, as well as the new balance.
+
+            //    - `Logout` - The user is logged out by setting `loggedInUserId` to -1, and the bank balance after logout is displayed.
+
+            //Note: This explanation assumes the `Bank` class and its methods(`Login`, `GetCustomerName`, `GetCustomerBalance`, `Deposit`, `Withdraw`) are implemented correctly.These methods are not visible in the provided code, so their exact behavior is inferred based on their names and usage.
+
+
+            // BEHAVIORS NOT IMPLEMENTED AND WHY: 
+
+
+            // Feature: Money transfer between users
+            // Reason: Not implemented due to time constraints and lower priority compared to other features.
 
             Bank bank = new Bank();
             Console.WriteLine($"Bank initial balance: {bank.BankBalance:C}");
